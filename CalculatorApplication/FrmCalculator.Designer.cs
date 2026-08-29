5775488e6c9af6681f4c7bb99298046ded36b2b2
Janella Peralta <peraltajanella1@gmail.com>
2026-08-29 15:56:45 +0800

    Add files via upload

---
+++ CalculatorApplication/FrmCalculator.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace CalculatorApplication
+{
+    partial class FrmCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSecondNumber = new System.Windows.Forms.TextBox();
+            this.txtFirstNumber = new System.Windows.Forms.TextBox();
+            this.cbOperator = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter First Number:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter Second Number:";
+            // 
+            // txtSecondNumber
+            // 
+            this.txtSecondNumber.Location = new System.Drawing.Point(158, 85);
+            this.txtSecondNumber.Name = "txtSecondNumber";
+            this.txtSecondNumber.Size = new System.Drawing.Size(141, 20);
+            this.txtSecondNumber.TabIndex = 2;
+            // 
+            // txtFirstNumber
+            // 
+            this.txtFirstNumber.Location = new System.Drawing.Point(158, 28);
+            this.txtFirstNumber.Name = "txtFirstNumber";
+            this.txtFirstNumber.Size = new System.Drawing.Size(141, 20);
+            this.txtFirstNumber.TabIndex = 3;
+            // 
+            // cbOperator
+            // 
+            this.cbOperator.FormattingEnabled = true;
+            this.cbOperator.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.cbOperator.Location = new System.Drawing.Point(196, 54);
+            this.cbOperator.Name = "cbOperator";
+            this.cbOperator.Size = new System.Drawing.Size(70, 21);
+            this.cbOperator.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(115, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Answer:";
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(166, 128);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(19, 13);
+            this.lblAnswer.TabIndex = 6;
+            this.lblAnswer.Text = "__";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(210, 151);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(89, 25);
+            this.btnCalculate.TabIndex = 7;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // FrmCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 197);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbOperator);
+            this.Controls.Add(this.txtFirstNumber);
+            this.Controls.Add(this.txtSecondNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmCalculator";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSecondNumber;
+        private System.Windows.Forms.TextBox txtFirstNumber;
+        private System.Windows.Forms.ComboBox cbOperator;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
