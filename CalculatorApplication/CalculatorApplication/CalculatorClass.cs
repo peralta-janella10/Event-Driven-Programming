2f76abb101e4fb324f23e3b9a396caf6b50e14bd
Janella Peralta <peraltajanella1@gmail.com>
2026-09-05 14:22:26 +0800

    Add files via upload

---
+++ CalculatorApplication/CalculatorApplication/CalculatorClass.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApplication
+{
+    public delegate T Information<T>(T arg1, T arg2);
+
+    internal class CalculatorClass
+    {
+        public Information<double> info;
+
+        private Information<double> calculateEvent;
+
+        public event Information<double> CalculateEvent
+        {
+            add
+            {
+                calculateEvent += value;
+                Console.WriteLine("Added the Delegate");
+            }
+            remove
+            {
+                calculateEvent -= value;
+                Console.WriteLine("Removed the Delegate");
+            }
+        }
+
+        public double RaiseCalculateEvent(double num1, double num2)
+        {
+            return calculateEvent?.Invoke(num1, num2) ?? 0.0;
+        }
+
+        public double GetSum(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double GetDifference(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double GetProduct(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public double GetQuotient(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
---
+++ CalculatorApplication/CalculatorApplication/FrmCalculator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorApplication
+{
+    public partial class FrmCalculator : Form
+    {
+
+        CalculatorClass cal;
+
+        public FrmCalculator()
+        {
+            InitializeComponent();
+
+            cal = new CalculatorClass();
+
+            cbOperator.Items.Clear();
+            cbOperator.Items.Add("+");
+            cbOperator.Items.Add("-");
+            cbOperator.Items.Add("*");
+            cbOperator.Items.Add("/");
+            cbOperator.SelectedIndex = 0;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(txtFirstNumber.Text);
+            double num2 = Convert.ToDouble(txtSecondNumber.Text);
+
+            string op = cbOperator.SelectedItem.ToString();
+
+            switch (op)
+            {
+                case "+":
+                    cal.CalculateEvent += cal.GetSum;
+                    break;
+                case "-":
+                    cal.CalculateEvent += cal.GetDifference;
+                    break;
+                case "*":
+                    cal.CalculateEvent += cal.GetProduct;
+                    break;
+                case "/":
+                    cal.CalculateEvent += cal.GetQuotient;
+                    break;
+            }
+
+            var total = cal.RaiseCalculateEvent(num1, num2);
+            lblAnswer.Text = total.ToString();
+
+            switch (op)
+            {
+                case "+":
+                    cal.CalculateEvent -= cal.GetSum;
+                    break;
+                case "-":
+                    cal.CalculateEvent -= cal.GetDifference;
+                    break;
+                case "*":
+                    cal.CalculateEvent -= cal.GetProduct;
+                    break;
+                case "/":
+                    cal.CalculateEvent -= cal.GetQuotient;
+                    break;
+            }
+        }
+    }
+}
